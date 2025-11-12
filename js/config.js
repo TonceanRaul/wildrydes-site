2640859fb986639a1e76dfa34896868b83693bbf
Raul Toncean <42072525+TonceanRaul@users.noreply.github.com>
2025-11-12 23:01:16 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_OxIHAlOCz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '50qs4mdd829lnr2t8b6qvrrqts', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '65ja0pkucgmb98fju8vh4kb1io', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
